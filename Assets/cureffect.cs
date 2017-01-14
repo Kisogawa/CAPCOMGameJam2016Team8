8eaa5c1f07c2ec92b8611a468fec7cc9ce9f2f26
DYNAMAC\dynamac <gumi.green.m793s@gmail.com>
2017-01-14 19:49:19 +0900

    カーソル追従エフェクト

---
+++ Assets/cureffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cureffect : MonoBehaviour {
+
+    private Vector3 pos;
+
+
+	// Use this for initialization
+	void Start () {
+        pos = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pos = Input.mousePosition;
+        pos.z = 30.0f;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        transform.position = pos;
+	}
+}
