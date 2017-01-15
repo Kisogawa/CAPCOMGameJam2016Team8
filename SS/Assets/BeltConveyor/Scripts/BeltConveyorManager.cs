d5ab40ac163b3a4dfb43763eb721effb9b47f08c
KisogawaNote <k-kkaam@live.jp>
2017-01-15 11:13:46 +0900

    ベルトコンベアのZ位置を変更

--- SS/Assets/BeltConveyor/Scripts/BeltConveyorManager.cs
+++ SS/Assets/BeltConveyor/Scripts/BeltConveyorManager.cs
@@ -6,14 +6,28 @@ public class BeltConveyorManager : MonoBehaviour {
 
 	public GameObject Conveyor;
 	public float Lengh;                 //ベルトコンベアの長さ
-	public float Speed;					//ベルトコンベアの速さ
+	public float Speed;                 //ベルトコンベアの速さ
+	public Vector3 Size;				//コンベアの大きさ
 
 	private void Start()
 	{
+
+		//メッシュのY方向の大きさを取得する
+		float sizeY = Size.x*8;
+
+		//全体の長さを計算する
+		float totalLen = Lengh * 2 + sizeY * Mathf.PI;
+
+		//一週回るのにかかる時間の計算
+		int totalCount = (int)((totalLen) / Speed);
+
+		//コンベアの必要個数を計算する
+		int num = (int)(totalLen / (sizeY*1.3));
+
 		//一気にスポーンする
-		for (int i = 0; i < Lengh * 3.7; i++)
+		for (int i = 0; i < num; i++)
 		{
-			Spown(i*60);
+			Spown(totalCount*i / num);
 		}
 	}
 
@@ -23,6 +37,7 @@ public class BeltConveyorManager : MonoBehaviour {
 		prefab.GetComponent<Conveyor>().Count = Count;
 		prefab.GetComponent<Conveyor>().Lengh = Lengh;
 		prefab.GetComponent<Conveyor>().Speed = Speed;
+		prefab.GetComponent<Conveyor>().Size = Size;
 
 	}
 }
